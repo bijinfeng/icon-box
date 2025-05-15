6236513fd700e5cd21489286e956fa7e9ef78b5d
bijinfeng <2804673379@qq.com>
2025-05-15 16:52:20 +0800

    feat: add logto

--- src/app/logto.ts
+++
@@ -1,8 +0,0 @@
-export const logtoConfig = {
-  endpoint: 'https://auth.fengrui.xyz/',
-  appId: 'voqlsjc521i4jtfg9a5xk',
-  appSecret: 'znPngFaszNfzp4L0HtaZEQR1vpVpYYsN',
-  baseUrl: 'http://localhost:3000', // Change to your own base URL
-  cookieSecret: 'lcKBTR0fTQCWke6uKtPeG8AHqjaz0CnU', // Auto-generated 32 digit secret
-  cookieSecure: process.env.NODE_ENV === 'production',
-};
---
+++ src/components/site/site-header.tsx
@@ -0,0 +1,36 @@
+import { signIn } from '@logto/next/server-actions';
+import { Button } from "@iconbox/ui/components/button"
+import { logtoConfig } from '@/config/logto';
+
+import { ModeToggle } from '@/components/theme/theme-switch'
+
+import { MainNav } from "./main-nav"
+
+export function SiteHeader() {
+  const onSignIn = async () => {
+    'use server';
+
+    await signIn(logtoConfig);
+  };
+
+  return (
+    <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container-wrapper">
+        <div className="container flex h-14 items-center gap-2 md:gap-4">
+          <MainNav />
+          <div className="ml-auto flex items-center gap-2 md:flex-1 md:justify-end">
+            <div className="hidden w-full flex-1 md:flex md:w-auto md:flex-none">
+              {/* <CommandMenu /> */}
+            </div>
+            <nav className="flex items-center gap-0.5">
+              <Button variant="ghost" size="sm" onClick={onSignIn}>
+                登录
+              </Button>
+              <ModeToggle variant="ghost" />
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
