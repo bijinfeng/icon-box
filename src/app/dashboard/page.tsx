fc1062aa3cc751578d954ca71c21830113b62cf9
kebai <2804673379@qq.com>
2025-05-27 13:53:06 +0800

    feat: 完善新建项目表单

--- src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC, PropsWithChildren } from "react";
-
-const Page: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="@container/main flex flex-1">{children}</div>;
-};
-
-export default Page;
--- src/app/dashboard/projects/new/page.tsx
+++ src/app/dashboard/projects/new/page.tsx
@@ -15,7 +15,10 @@ import {
 } from '@iconbox/ui/components/form'
 import { Input } from '@iconbox/ui/components/input'
 import { Textarea } from '@iconbox/ui/components/textarea'
+import { RadioGroup, RadioGroupItem } from '@iconbox/ui/components/radio-group'
+import { Label } from '@iconbox/ui/components/label'
 import { OwnerSelect } from "@/components/dashboard/sidebar/owner-select";
+import { useId } from "react";
 
 const formSchema = z.object({
   owner: z.string().min(2, {
@@ -25,15 +28,23 @@ const formSchema = z.object({
     message: "Project name must be at least 2 characters.",
   }),
   description: z.string().optional(),
+  prefix: z.string().optional(),
+  fontFamily: z.string().optional(),
+  visibility: z.enum(['public', 'private']),
 });
 
 export default function NewProject() {
+  const radioId = useId()
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       owner: "",
       name: "",
       description: "",
+      prefix: 'icon-',
+      fontFamily: 'iconbox',
+      visibility: 'private',
     },
   });
 
@@ -44,7 +55,7 @@ export default function NewProject() {
   }
 
   return (
-    <div className="lg:max-w-2xl mt-10 mx-auto w-full">
+    <div className="lg:max-w-2xl py-10 mx-auto w-full">
       <div className="space-y-6">
         <div>
           <h3 className="text-lg font-medium">新建项目</h3>
@@ -53,7 +64,6 @@ export default function NewProject() {
           </p>
         </div>
         <Separator />
-
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="flex items-end">
@@ -62,7 +72,7 @@ export default function NewProject() {
                 name="owner"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Owner</FormLabel>
+                    <FormLabel className="flex gap-1">Owner <span>*</span></FormLabel>
                     <FormControl>
                       <OwnerSelect {...field} />
                     </FormControl>
@@ -76,7 +86,7 @@ export default function NewProject() {
                 name="name"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>项目名称</FormLabel>
+                    <FormLabel className="flex gap-1">项目名称 <span>*</span></FormLabel>
                     <FormControl>
                       <Input className="h-8" {...field} />
                     </FormControl>
@@ -92,12 +102,86 @@ export default function NewProject() {
                 <FormItem>
                   <FormLabel>项目描述</FormLabel>
                   <FormControl>
-                    <Textarea {...field} />
+                    <Textarea {...field} placeholder="请输入项目描述（可选）" />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
+
+            <Separator />
+
+            <FormField
+              control={form.control}
+              name="prefix"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>FontClass / Symbol 前缀</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="请输入 FontClass/Symbol 前缀" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="fontFamily"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Font Family</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="请输入 Font Family，默认 iconbox" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Separator />
+            <FormField control={form.control} name="visibility" render={({ field }) => (
+              <RadioGroup className="gap-6" value={field.value} onValueChange={field.onChange}>
+                <div className="flex items-start gap-2">
+                  <RadioGroupItem
+                    value="public"
+                    id={`${radioId}-1`}
+                    aria-describedby={`${radioId}-1-description`}
+                  />
+                  <div className="grid grow gap-2">
+                    <Label htmlFor={`${radioId}-1`}>
+                      Public
+                    </Label>
+                    <p
+                      id={`${radioId}-1-description`}
+                      className="text-muted-foreground text-xs"
+                    >
+                      You can use this card with a label and a description.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-start gap-2">
+                  <RadioGroupItem
+                    value="private"
+                    id={`${radioId}-2`}
+                    aria-describedby={`${radioId}-2-description`}
+                  />
+                  <div className="grid grow gap-2">
+                    <Label htmlFor={`${radioId}-2`}>
+                      Private
+                    </Label>
+                    <p
+                      id={`${radioId}-2-description`}
+                      className="text-muted-foreground text-xs"
+                    >
+                      You can use this card with a label and a description.
+                    </p>
+                  </div>
+                </div>
+              </RadioGroup>
+            )} />
+
+            <Separator />
+
             <Button type="submit">新建项目</Button>
           </form>
         </Form>
