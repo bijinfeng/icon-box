268b2af0556dfdc569b1f5c9bc0fc2388b88cd0c
kebai <2804673379@qq.com>
2025-06-04 18:02:16 +0800

    feat: add user model

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -1,11 +1,52 @@
-// import { Database } from "../type";
+import { eq } from 'drizzle-orm';
 
-// export class UserModel {
-//   private userId: string;
-//   private db: Database;
+import { Database } from "../type";
+import { type NewUser, users, type UserItem } from '../schemas';
 
-//   constructor(db: Database, userId: string) {
-//     this.userId = userId;
-//     this.db = db;
-//   }
-// }
+export class UserModel {
+  private userId: string;
+  private db: Database;
+
+  constructor(db: Database, userId: string) {
+    this.userId = userId;
+    this.db = db;
+  }
+
+  updateUser = async (value: Partial<UserItem>) => {
+    return this.db
+      .update(users)
+      .set({ ...value, updatedAt: new Date() })
+      .where(eq(users.id, this.userId));
+  };
+
+  static makeSureUserExist = async (db: Database, userId: string) => {
+    await db.insert(users).values({ id: userId }).onConflictDoNothing();
+  };
+
+  static createUser = async (db: Database, params: NewUser) => {
+    // if user already exists, skip creation
+    if (params.id) {
+      const user = await db.query.users.findFirst({ where: eq(users.id, params.id) });
+      if (!!user) return { duplicate: true };
+    }
+
+    const [user] = await db
+      .insert(users)
+      .values({ ...params })
+      .returning();
+
+    return { duplicate: false, user };
+  };
+
+  static deleteUser = async (db: Database, id: string) => {
+    return db.delete(users).where(eq(users.id, id));
+  };
+
+  static findById = async (db: Database, id: string) => {
+    return db.query.users.findFirst({ where: eq(users.id, id) });
+  };
+
+  static findByEmail = async (db: Database, email: string) => {
+    return db.query.users.findFirst({ where: eq(users.email, email) });
+  };
+}
--- src/database/schemas/user.ts
+++ src/database/schemas/user.ts
@@ -1,9 +1,29 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { boolean, jsonb, pgTable, text } from 'drizzle-orm/pg-core';
 
-export const userSettings = pgTable('user_settings', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+import { timestamps, timestamptz } from './_helpers';
+
+export const users = pgTable('users', {
+  id: text('id').primaryKey().notNull(),
+  username: text('username').unique(),
+  email: text('email'),
+
+  avatar: text('avatar'),
+  phone: text('phone'),
+  firstName: text('first_name'),
+  lastName: text('last_name'),
+  fullName: text('full_name'),
+
+  isOnboarded: boolean('is_onboarded').default(false),
+  // Time user was created in Clerk
+  clerkCreatedAt: timestamptz('clerk_created_at'),
+
+  // Required by nextauth, all null allowed
+  emailVerifiedAt: timestamptz('email_verified_at'),
+
+  preference: jsonb('preference'),
+
+  ...timestamps,
 });
-export type UserSettingsItem = typeof userSettings.$inferSelect;
+
+export type NewUser = typeof users.$inferInsert;
+export type UserItem = typeof users.$inferSelect;
